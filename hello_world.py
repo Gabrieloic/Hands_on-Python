9f14ec0380ca0acad130b7af1e42f9d31851bf77
Gabrieloic <loic.gabs237@gmail.com>
2025-03-09 13:27:35 +0100

    Add changes to hello_world.py and new files

--- hello_world.py
+++ hello_world.py
@@ -201,8 +201,185 @@
 
 # 
 # on peut aussi verifier le type d'une chaine avec les differentes methodes
-print("bonjour Le mondE".islower())
+# print("bonjour Le mondE".islower())
+# va retourner false parce que toute la chaine n'est pas lower
+# print("bonjour Le mondE".isupper())
+# va retourner False parce que toute la chaine n'est pas upper
+# print("BONJOUR LE MONDE".isupper())
+# # va retourner True parce que toute la chaine verifier la condition
 
-print("bonjour Le mondE".isupper())
+# Les Dictionnaires
+# un dico permet d'associer des cles et des valeurs
 
-print("BONJOUR LE MONDE".isupper())
+# on cree un dico de cette facon
+# personne = {
+#     "nom" : "Gabriel",
+#     "age" : 29,
+#     "ville": "Paris"
+# }
+# print(personne["nom"])
+
+# on peut modifier une cle en lui donnant un autre valeur
+# ce qui va ecraser la premiere
+# personne ["age"] = 33
+# print(personne["age"])
+
+# on peut aussi ajouter un nouvel element
+# personne["email"] = "contact@dogfather.com"
+# print(personne["email"])
+
+# pour parcourir tout le contenu d'un dictionnaire on fait une boucle
+
+# for cle, valeur in personne.items():
+#     print(f"{cle} : {valeur}")
+
+# le " f " permet d'interpreter tout ce qui est dans la methode print comme una variable ou une expression
+
+# on a ca en resultat
+# nom : Gabriel
+# age : 33
+# ville : Paris
+# email : contact@dogfather.com
+
+# les tuples
+
+# les tuples c'est une liste qui est imuable c.a.d qui ne peut pas etre modifie une fois creee
+# ce qui peut etre interessant pour securiser des donnees et empecher des modifications excessives
+# comme par exemple des cles API, on prefere les mettre dans un tuple plutot qu'une liste
+
+# prenom = ("Samuel", "lucas", "Romain","Gabriel")
+# print(prenom[2])
+# etant donne que les id des elements se comptent a partir de 0, on peut donc afficher le 3 eme element ainsi
+
+
+# ecrire dans des fichiers
+
+# pour le faire on utilise la methode "open"
+
+# with open("mon_fichier_test.txt", "w") as fichier:
+#     fichier.write("Salut la famille, ceci est la premiere ligne de mon fichier rajoutee avec la methode open sur Python")
+
+# le " w " qu'on met dans la syntaxe permet de dire qu'on va ecrire, mais s'il y a deja du contenu
+#  dans le fichier il sera ecrase
+# Aussi avec la methode open, si le fichier n'existe pas, il sera cree dans le meme dossier que 
+# notre script Python
+
+
+# Pour ajouter du contenu dans un fichier sans supprimer l'existant
+# cette fois c'est avec la lettre " a "
+# with open("mon_fichier_test.txt", "a") as fichier:
+#     fichier.write("\nCeci est la nouvelle ligne que je rajoute dans mon fichier")
+
+
+# Lire un fichier 
+
+# with open("mon_fichier_test.txt", "r") as fichier:
+#     contenu = fichier.read()
+#     print(contenu)
+# le " r " ici est mis pour read et donc avec cette methode on affiche tout le contenu d'un fichier
+# dans le terminal un peut comme une commande cat sur linux
+
+
+# Gerer les erreurs
+
+# prenons un cas on demande a un user de donner un nombre au clavier mais au lieu d'un nombre il met
+# une lettre, automatiquement python va me retourner une erreur
+# Il faut donc un moyen de dire au user qu'il a fait une erreur en ne mettant pas le bon type demande
+
+# nombre = int(input("donner moi un nombre : "))
+# print("le triple de votre nombre est :", nombre*3)
+
+# jusqu'ici on est bon si le user met un chiffre, 
+# mais si le user met une lettre a la place on aura cette erreur
+# Python/hello_world.py
+# donner moi un nombre : ret
+# Traceback (most recent call last):
+#   File "/Users/the-dogfather/Documents/Devops/Hands_on-Python/hello_world.py", line 289, in <module>
+#     nombre = int(input("donner moi un nombre : "))
+# ValueError: invalid literal for int() with base 10: 'ret'
+# l'erreur dit que le valeur entree n'est pas du meme type que celle attendue
+# dans ce cas pour la resourdre on utilise la methode " try except "
+
+# try:
+#     nombre = int(input("donner moi un nombre : "))
+#     print("le triple de votre nombre est :", nombre*3)
+# except ValueError:
+#     print("Attention vous n'avez pas taper un nombre valide !!!!! ")
+
+
+# Importer des modules et bibliotheques
+
+# import math
+# print(math.sqrt(25))
+# on importe la biblio math pour faire le calcul de la racine caree
+
+# on peut importe une simple fonction d'une bibliothe plutot que la bibliotheque entiere
+
+# from random import randint
+# print(randint(1,45)) 
+# cette fonction me permet de generer un nombre aleatoire entre la selection que je specifie
+
+# on peut aussi importer des modules supplementaires qui ne sont pas presentes nativement par defaut sur python 
+
+# on peut intaller une nouvelle bibliotheque depuis notre cli en dehors de l'interpreteur python
+# requests est une lib qui permet d'envoyer des requettes vers un site web histoire de tester la connexion
+#  en fonction de la reponse
+# error 200 : Ca veut dire que le site est atteignable
+# error 404 : des chances que le site n'existe plus ...etc 
+
+# import requests
+# reponse = requests.get("https://www.google.com")
+# print(reponse.status_code)
+
+
+# Exercice
+
+# 1: on cree un dico avec ( nom, age, ville)
+# 2: on ecris le dico dans un fichier txt
+# 3: utiliser try except pour eviter des erreurs 
+# 4: on lit et affiche le contenu de notre fichier
+# 5: on demande d'utiliser un module pour donner un chiffre aleatoire
+
+# Solution 
+
+# 1: on cree un dico avec ( nom, age, ville)
+
+# on cree le dico
+# personne = {
+#     "nom": "Lydie",
+#     "prenom": "sam",
+#     "age": 25,
+#     "email": "sam_lydie@mugiwara.op",
+#     "ville": "Nice"
+# }
+# on affiche en balayant tous les elements 
+# for cle, valeur in personne.items():
+#     dico = print(f"{cle} : {valeur}")
+
+# 2: on ecris le dico dans un fichier txt
+# 3: utiliser try except pour eviter des erreurs, dans le cas ou le fichier est inexistant
+
+# try:
+#     with open("mon_fichier_2.txt", "w") as fichier:
+#         for cle, valeur in personne.items():
+#             fichier.write(f"{cle} : {valeur} \n")
+#     print("\nc'est dans la boite !!! \n ")
+# except Exception as e:
+#     print("aie aie aie probleme !!! ")
+
+# 4: on lit et affiche le contenu de notre fichier
+
+# try:
+#     with open("mon_fichier_3.txt", "r") as fichier:
+#         print(fichier.read())
+# except FileNotFoundError:
+#     print("aie aie aie le fichier n'existe pas !!\n ")
+
+# 5: on demande d'utiliser un module pour donner un chiffre aleatoire
+
+# from random import randint
+# print("nombre aleatoire entre 1 et 90 : ", randint(1,90))
+
+
+a = "pyth"
+print(a[-5])
